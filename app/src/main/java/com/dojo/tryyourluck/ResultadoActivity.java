fb3348b8c3d396d12e9abd5d5797d31ba9cc5f91
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-04-29 17:07:55 -0300

    Jogo de cara ou coroa

--- app/src/main/java/com/dojo/tryyourluck/ResultadoActivity.java
+++ app/src/main/java/com/dojo/tryyourluck/ResultadoActivity.java
@@ -3,22 +3,38 @@ package com.dojo.tryyourluck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imageResultado;
+    private ImageButton buttonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
 
+        imageResultado = findViewById(R.id.imageResultado);
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+
         Bundle dados = getIntent().getExtras();
 
         int numero = dados.getInt("numero");
 
         if (numero == 0) {
-
+            imageResultado.setImageResource(R.drawable.moeda_cara);
         }else{
-
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
         }
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
